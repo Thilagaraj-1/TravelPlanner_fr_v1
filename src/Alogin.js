a7068daec6d0c8c3a64ec2ab6ea3441543ecb773
Thilagaraj-1 <160594508+Thilagaraj-1@users.noreply.github.com>
2024-03-13 13:30:23 +0530

    changes

--- src/Alogin.js
+++ src/Alogin.js
@@ -1,7 +1,19 @@
 function Alogin(){
     return(
-        <div>
-            
+        <div className="container">
+            <h2 className="mt-4 mb-3">Admin Login</h2>
+            <form>
+                <div className="form-group pb-3">
+                    <label htmlFor="password" className="pb-1 ps-2">Admin</label>
+                    <input type="text" name="adminName" className="form-control" id="adminName" placeholder="AdminName"/>
+                </div>
+                <div className="form-group pb-3">
+                    <label htmlFor="password" className="pb-1 ps-2">Password</label>
+                    <input type="password" name="password" className="form-control" id="password" placeholder="Password"/>
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>  
+           
         </div>
     )
 }
